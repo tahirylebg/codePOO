e6990bfa5abf5147349c4ef4cf83736fc64c2ee3
tahirylebg <tahiry.ramambazafy@ynov.com>
2026-02-15 12:55:11 +0100

    Avancement du systeme de combat

--- Combat/CombatSystem.php
+++ Combat/CombatSystem.php
@@ -11,6 +11,7 @@ class CombatSystem{
 
     // Vérifie si tous les monstres sont morts
     private function allMonstersDead(array $monsters): bool{
+        // Parcourt tous les monstres et vérifie s'ils sont tous morts
         foreach ($monsters as $monster){
             if (!$monster->isDead()){
                 return false;
@@ -19,5 +20,25 @@ class CombatSystem{
         return true;
     }
 
-    
+    //Le combat se déroule en tours, où le héros attaque d'abord, suivi des attaques des monstres encore en vie.
+    public function startCombat(IHero $hero , array $monsters): bool {
+        while (!$hero -> isDead() !$this->allMonstersDead($monsters)){
+
+            
+        }
+    }
+
+
+    // Tour du héros : le héros attaque le premier monstre vivant dans la liste
+    private function heroTurn(IHero $hero, array $monsters): void{
+        // Le héros attaque le premier monstre vivant dans la liste
+        foreach ($monsters as $monster){
+            if (!$monster->isDead()){
+                $hero->attack($monster);
+                break; // Le héros attaque un seul monstre par tour, donc on sort de la boucle après la première attaque
+            }
+        }
+    }
+
+
 }
